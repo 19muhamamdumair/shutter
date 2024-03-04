b3716fda1450ef740604fac669e8968bb59d6f39
Shoaib Imran <47287416+ShoaibImran@users.noreply.github.com>
2024-03-04 01:46:51 +0000

    further updated on component step 2

--- app/components/modals/RentModal.tsx
+++ app/components/modals/RentModal.tsx
@@ -8,6 +8,7 @@ import Heading from "../Heading";
 import { categories } from "../navbar/Categories";
 import CategoryInput from "../inputs/CategoryInput";
 import { FieldValues, useForm } from "react-hook-form";
+import CountrySelect from "../inputs/CountrySelect";
 
 enum STEPS {
     CATEGORY = 0,
@@ -103,11 +104,26 @@ const RentModal = () => {
         </div>
     )
 
+    if (step == STEPS.LOCATION) {
+        bodyContent = (
+            <div className="flex flex-col gap-8 ">
+                <Heading 
+                    title="Where are you located?"
+                    subtitle="Help potential clients find you!"
+                />
+                <CountrySelect 
+                    
+                
+                />
+            </div>
+        )
+    }
+
     return (
         <Modal 
             isOpen={rentModal.isOpen}
             onClose={rentModal.onClose}
-            onSubmit={rentModal.onClose}
+            onSubmit={onNext}
             actionLabel={actionLabel}
             secondaryActionLabel={secondaryActionLabel}
             secondaryAction={step == STEPS.CATEGORY ? undefined : onBack}
